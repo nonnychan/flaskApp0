697c8c4880f199bb30ee59e01bccd84c45b23991
nonnychan <nonnychan2545@gmail.com>
2022-11-29 15:08:52 +0700

    serving JSON

--- app/views.py
+++ app/views.py
@@ -1,6 +1,23 @@
+from flask import jsonify
 from app import app
  
  
 @app.route('/')
 def home():
-    return "Nonny says 'Hello world!'"
+    return "Flask says 'Hello world!'"
+ 
+ 
+@app.route('/phonebook')
+def index():
+    return app.send_static_file('phonebook.html')
+
+
+# This route serves the dictionary d at the route /date
+@app.route("/api/data")
+def data():
+    # define some data
+    d = {
+        "Alice": "(708) 727-2377",
+        "Bob": "(305) 734-0429"
+    }
+    return jsonify(d)  # convert your data to JSON and return
